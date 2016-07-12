afcd8bd8d61a5720e15cf316b20529d10732cab4
majascules <khaksari@gmail.com>
2016-07-12 17:54:19 -0500

    Add files via upload
    OO-ing the script, switching from sys.arg to argparse, detecting and auto installing tqdm if missing, optional reporting of status messages based  on --verbose flag, defaults for output if none specified, handling working directory filenames.

--- transpose.py
+++ transpose.py
@@ -2,6 +2,7 @@ import csv
 import os
 import sys
 import time
+from tqdm import *
 
 #Setup Timer Variables
 start_time=time.time()
@@ -20,20 +21,26 @@ f = open(sys.argv[1])
 #Parse 2nd STDIN Arguemnt as Output Filename
 output = open(sys.argv[2],'w')
 
-#Instantiate CSV Reader
+#Instantiate CSV Reader and the read in data
 csv_f = csv.reader(f)
+data=list(csv_f)
 
 #Instantiate CSV Writer
 wr=csv.writer(output, quoting=csv.QUOTE_ALL)
 
+#Count Rows in Input File
+row_count = len(data)
+
 #Parse Input file and Get Name/Value Tuplets
-for row in csv_f:
+#using tqdm wrapper to provide a status bar
+#for row in csv_f:
+for row in tqdm(data,total=row_count):
     
     #Decode URL, Parse URL for Path, and truncate to filename
     #Then Add filename to array filenames
     key = (row[0])
     value = (row[1])
-    values = value.split("|")
+    values = value.split(" | ")
     #values = np.array(value)
     for v in values:
         row=key+v
